f2519875bc1244901e48a9d383cadc95eaa6415f
anupyadavdushad <anupyadavdushad@gmail.com>
2026-01-04 21:00:28 +0530

    Updated embedder.py by giving the imges in batches of 1 beacuse model just only take data in batches even if it is single image

--- src/recognition/embedder.py
+++ src/recognition/embedder.py
@@ -1,37 +1,40 @@
 import cv2
 import numpy as np
-from numpy.linalg import norm 
+from numpy.linalg import norm
 from keras_facenet import FaceNet
 
 class FaceEmbedder:
-    def __init__(self, model=None):
-        """
-        model: uses FaceNet from keras_facenet
-        """
-        if model is None:
-            self.model = FaceNet()
+    def __init__(self):
+        self.model = FaceNet()
 
+    def preprocess(self, img):
+        # Force valid image
+        if img is None:
+            raise ValueError("Input image is None")
 
-    def preprocess(self, face_img):
-        """
-        Input: BGR face image (OpenCV)
-        Output: normalized RGB tensor
-        """
+        # # Handle grayscale images
+        # if len(img.shape) == 2:
+        #     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-        img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
+        # # Handle RGBA just in case
+        # if img.shape[2] == 4:
+        #     img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+        # BGR -> RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Resize
         img = cv2.resize(img, (160, 160))
-        img = img.astype("float32")
-        img = (img - 127.5) / 128.0
-        return img
-    
 
-    def get_embedding(self, face_img):
-        """
-        Returns L2-normalized embedding vector
-        """
-        preprocessed = self.preprocess(face_img)
-        embedding = self.model.embeddings(preprocessed)[0]
+        return img
+
+    def get_embedding(self, img):
+        img = self.preprocess(img)
+
+        # IMPORTANT: batch dimension
+        batch = np.expand_dims(img, axis=0)  # (1, 160, 160, 3)
+
+        embedding = self.model.embeddings(batch)[0]
         embedding = embedding / norm(embedding)
-        
-        return embedding 
-    
+
+        return embedding
